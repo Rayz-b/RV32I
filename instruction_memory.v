147f14c97533dd5874b86aed30c89ae51d6cb7ce
Rayz-b <92997215+Rayz-b@users.noreply.github.com>
2021-11-07 19:04:53 +0500

    loading code from hexdump using $readmemh

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,44 +1,43 @@
 module instruction_memory (output reg [31:0] Instr, input [31:0] PC);
+    reg [31:0] instr_mem [44:0];
+    
+    initial
+    $readmemh("INSTRUCTION_MEM.mem", instr_mem);
+    
     always@(PC) begin
-        case (PC)
-		0: Instr<=32'h04000413;
-		4: Instr<=32'h03200493;
-//		8: Instr<=32'h008000a3;//sb
-//		12: Instr<=32'h009001a3;//sb
-		8: Instr<=32'h00801023;//sh
-		12: Instr<=32'h00901123;//sh
-//		8: Instr<=32'h00802023;//sw
-//		12:Instr<=32'h00902223;//sw
-//		16: Instr<=32'h00100483;//lb
-//		20: Instr<=32'h00300403;//lb
-		16: Instr<=32'h00001483;//lh
-		20: Instr<=32'h00201403;//lh
-//		16:Instr<=32'h00002483;//lw
-//		20:Instr<=32'h00402403;//lw
-		16: Instr<=32'h00005483;//lhu
-		20: Instr<=32'h00005483;//lbu
-		24: Instr<=32'h00940e63;
-		28: Instr<=32'h00944663;
-		32: Instr<=32'h40940433;
-	        36: Instr<=32'hff5ff06f;
-	        40:	Instr<=32'h408484b3;
-	        44:	Instr<=32'hfedff06f;
-	        48:	Instr<=32'h00900123;
-	        52:	Instr<=32'h0000006f;
-		default: Instr<=32'd0;
-        endcase
+        Instr <= instr_mem[PC>>2];
     end
-
-
-
-
-
-
-
 
 
 	
 endmodule
+//        case (PC)
+//		0: Instr<=32'h04000413;
+//		4: Instr<=32'h03200493;
+////		8: Instr<=32'h008000a3;//sb
+////		12: Instr<=32'h009001a3;//sb
+//		8: Instr<=32'h00801023;//sh
+//		12: Instr<=32'h00901123;//sh
+////		8: Instr<=32'h00802023;//sw
+////		12:Instr<=32'h00902223;//sw
+////		16: Instr<=32'h00100483;//lb
+////		20: Instr<=32'h00300403;//lb
+//		16: Instr<=32'h00001483;//lh
+//		20: Instr<=32'h00201403;//lh
+////		16:Instr<=32'h00002483;//lw
+////		20:Instr<=32'h00402403;//lw
+//		16: Instr<=32'h00005483;//lhu
+//		20: Instr<=32'h00005483;//lbu
+//		24: Instr<=32'h00940e63;
+//		28: Instr<=32'h00944663;
+//		32: Instr<=32'h40940433;
+//	        36: Instr<=32'hff5ff06f;
+//	        40:	Instr<=32'h408484b3;
+//	        44:	Instr<=32'hfedff06f;
+//	        48:	Instr<=32'h00900123;
+//	        52:	Instr<=32'h0000006f;
+//		default: Instr<=32'd0;
+//        endcase
 
 //ASSEMBLY
 //     addi x8 , x0 , 12
