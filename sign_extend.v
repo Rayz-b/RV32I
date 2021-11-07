b9a0105a55aa7f3094e94c14d9f98961b1d1f157
Rayz-b <92997215+Rayz-b@users.noreply.github.com>
2021-11-07 20:56:59 +0500

    Update sign_extend.v

--- sign_extend.v
+++ sign_extend.v
@@ -7,7 +7,7 @@ module extend(output reg 	[31:0] immext,
             3'b000:     immext = {{20{instr[31]}}, instr[31:20]};// I-type
             3'b001:     immext = {{20{instr[31]}}, instr[31:25], instr[11:7]};// S-type (stores)
             3'b010:     immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};// B-type (branches)
-            3'b011:     immext = {{12{instr[31]}}, instr[30:12], 1'b0};// U-type 
+            3'b011:     immext = {instr[31:12], 12'b0};// U-type                //examine later for wire alignment optimization
             3'b100:     immext = {{12{instr[31]}}, instr[19:12],  instr[20], instr[30:21], 1'b0};// J-type       
             default:   immext = 32'bx; // undefined
         endcase
