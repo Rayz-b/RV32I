959cee80bcdd738b5df41010d0b549d79a97844e
Rayz-b <92997215+Rayz-b@users.noreply.github.com>
2021-11-08 18:11:38 +0500

    removed outdated signals

--- RV32I.v
+++ RV32I.v
@@ -21,7 +21,7 @@
 
 //output reg [6:0] LED, output reg [7:0] Anode, input clk,rst,user_wr,user_sel, input [6:0] user_in
 module RV32I(output [31:0] Result, input clk,rst);
-    wire [6:0] opcode,user_out;
+    wire [6:0] opcode;
     wire [14:12] funct3; 
     wire [31:25] funct7; 
     wire [31:0] Instr;
@@ -30,6 +30,6 @@ module RV32I(output [31:0] Result, input clk,rst);
     wire [2:0] ImmSrc, br_type, ReadControl,WriteControl;
     wire [1:0] wb_sel;
     
-    datapath dp(opcode,user_out, funct3, funct7, Result, rst,clk,reg_wr,sel_A,sel_B,user_wr,user_sel, wb_sel, ImmSrc, alu_op,br_type,ReadControl,WriteControl,user_in);
+    datapath dp(opcode, funct3, funct7, Result, rst,clk,reg_wr,sel_A,sel_B, wb_sel, ImmSrc, alu_op,br_type,ReadControl,WriteControl);
     controller con(ImmSrc,alu_op,br_type,ReadControl,WriteControl, reg_wr, sel_A, sel_B, wb_sel, opcode, funct3, funct7,rst);
 endmodule
